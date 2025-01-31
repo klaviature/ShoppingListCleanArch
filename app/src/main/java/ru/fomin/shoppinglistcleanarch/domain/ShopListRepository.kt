52dda672cd721a8d1bff77735c7c3fe2113c15f3
Egor Fomin <egorfomin8158@gmail.com>
2025-01-31 22:59:54 +0500

    Creating repository

---
+++ app/src/main/java/ru/fomin/shoppinglistcleanarch/domain/ShopListRepository.kt
@@ -0,0 +1,14 @@
+package ru.fomin.shoppinglistcleanarch.domain
+
+interface ShopListRepository {
+
+    fun getShopList(): List<ShopItem>
+
+    fun getShopItem(shopItemId: Int): ShopItem
+
+    fun addShopItem(shopItem: ShopItem)
+
+    fun editShopItem(shopItem: ShopItem)
+
+    fun deleteShopItem(shopItem: ShopItem)
+}
