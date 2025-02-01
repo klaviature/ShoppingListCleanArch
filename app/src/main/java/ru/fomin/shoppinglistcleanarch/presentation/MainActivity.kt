3b87278854f2df65c192de3c34828510bedc2268
Egor Fomin <egorfomin8158@gmail.com>
2025-02-02 03:01:44 +0500

    Add shop list LiveData and Get, Delete and Toggle methods in main view model

--- app/src/main/java/ru/fomin/shoppinglistcleanarch/presentation/MainActivity.kt
+++ app/src/main/java/ru/fomin/shoppinglistcleanarch/presentation/MainActivity.kt
@@ -1,13 +1,20 @@
 package ru.fomin.shoppinglistcleanarch.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.fomin.shoppinglistcleanarch.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +24,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        viewModel.shopListLiveData.observe(this) { shopList ->
+            Log.d(TAG, shopList.toString())
+        }
+
+        viewModel.getShopList()
     }
 }
--- app/src/main/java/ru/fomin/shoppinglistcleanarch/presentation/MainViewModel.kt
+++ app/src/main/java/ru/fomin/shoppinglistcleanarch/presentation/MainViewModel.kt
@@ -1,10 +1,13 @@
 package ru.fomin.shoppinglistcleanarch.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.fomin.shoppinglistcleanarch.data.ShopListRepositoryImpl
 import ru.fomin.shoppinglistcleanarch.domain.DeleteShopItemUseCase
 import ru.fomin.shoppinglistcleanarch.domain.EditShopItemUseCase
 import ru.fomin.shoppinglistcleanarch.domain.GetShopListUseCase
+import ru.fomin.shoppinglistcleanarch.domain.ShopItem
 
 class MainViewModel : ViewModel() {
 
@@ -15,4 +18,23 @@ class MainViewModel : ViewModel() {
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
 
     private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    private val _shopListLiveData = MutableLiveData<List<ShopItem>>()
+    val shopListLiveData: LiveData<List<ShopItem>> get() = _shopListLiveData
+
+    fun getShopList() {
+        val shopList = getShopListUseCase.getShopList()
+        _shopListLiveData.postValue(shopList)
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun toggleShopItemEnabled(shopItem: ShopItem) {
+        val toggledShopItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(toggledShopItem)
+        getShopList()
+    }
 }
