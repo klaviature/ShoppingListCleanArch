1f551e3866b2d185eb09b4171110f339d7c17b3d
Egor Fomin <egorfomin8158@gmail.com>
2025-02-02 02:29:54 +0500

    Changing compile sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ru.fomin.shoppinglistcleanarch"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.fomin.shoppinglistcleanarch"
