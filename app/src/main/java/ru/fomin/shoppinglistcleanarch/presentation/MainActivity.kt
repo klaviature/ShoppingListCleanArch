5787ebd49f86b0aa850675739394ebf35178f8be
Egor Fomin <egorfomin8158@gmail.com>
2025-02-03 22:04:41 +0500

    Refactor shop list RV adapter

--- app/src/main/java/ru/fomin/shoppinglistcleanarch/presentation/MainActivity.kt
+++ app/src/main/java/ru/fomin/shoppinglistcleanarch/presentation/MainActivity.kt
@@ -2,33 +2,44 @@ package ru.fomin.shoppinglistcleanarch.presentation
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.fomin.shoppinglistcleanarch.R
-import ru.fomin.shoppinglistcleanarch.domain.ShopItem
+import ru.fomin.shoppinglistcleanarch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
 
+    private lateinit var binding: ActivityMainBinding
+
     private val viewModel by viewModels<MainViewModel>()
+
+    private lateinit var adapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        setupAdapter()
         viewModel.shopListLiveData.observe(this) { shopList ->
             Log.d(TAG, shopList.toString())
+            adapter.shopList = shopList
         }
+    }
+
+    private fun setupAdapter() {
+        adapter = ShopListAdapter()
+        binding.shopListRecyclerView.adapter = adapter
     }
 }
--- app/src/main/java/ru/fomin/shoppinglistcleanarch/presentation/ShopListAdapter.kt
+++ app/src/main/java/ru/fomin/shoppinglistcleanarch/presentation/ShopListAdapter.kt
@@ -12,23 +12,36 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
 
     inner class ShopItemViewHolder(val view: View) :
         RecyclerView.ViewHolder(view) {
-            val nameTextView: TextView = view.findViewById<TextView>(R.id.shopItemNameTextView)
-            val countTextView: TextView = view.findViewById<TextView>(R.id.shopItemCountTextView)
+        val nameTextView: TextView = view.findViewById<TextView>(R.id.shopItemNameTextView)
+        val countTextView: TextView = view.findViewById<TextView>(R.id.shopItemCountTextView)
     }
 
-    private val items = listOf<ShopItem>()
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = shopList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_enabled, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_enabled,
+            parent,
+            false
+        )
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        val item = items[position]
-        holder.nameTextView.text = item.name
-        holder.countTextView.text = String.format(item.count.toString())
+        val shopItem = shopList[position]
+        val status = if (shopItem.enabled) {
+            "Enabled"
+        } else {
+            "Disabled"
+        }
+        holder.nameTextView.text = "${shopItem.name} ($status)"
+        holder.countTextView.text = String.format(shopItem.count.toString())
         holder.view.setOnLongClickListener {
             true
         }
