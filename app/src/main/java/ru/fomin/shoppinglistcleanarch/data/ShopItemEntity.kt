5780e53a0b48b7dea53ad1fac0e35dc9f6a09af5
egork <egorfomin8158@gmail.com>
2025-02-22 19:26:43 +0500

    Create an ShopItemEntity class

---
+++ app/src/main/java/ru/fomin/shoppinglistcleanarch/data/ShopItemEntity.kt
@@ -0,0 +1,18 @@
+package ru.fomin.shoppinglistcleanarch.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shop_items")
+data class ShopItemEntity(
+    val name: String,
+    val count: Int,
+    val enabled: Boolean,
+    @PrimaryKey(autoGenerate = true) var id: Int = UNDEFINED_ID
+) {
+
+    companion object {
+
+        const val UNDEFINED_ID = -1
+    }
+}
--- app/src/main/java/ru/fomin/shoppinglistcleanarch/domain/ShopItem.kt
+++ app/src/main/java/ru/fomin/shoppinglistcleanarch/domain/ShopItem.kt
@@ -1,14 +1,11 @@
 package ru.fomin.shoppinglistcleanarch.domain
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity(tableName = "shop_items")
 data class ShopItem(
     val name: String,
     val count: Int,
     val enabled: Boolean,
-    @PrimaryKey var id: Int = UNDEFINED_ID
+    var id: Int = UNDEFINED_ID
 ) {
 
     companion object {
